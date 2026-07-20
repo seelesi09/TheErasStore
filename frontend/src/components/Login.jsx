dad3b1231f2b09462623060970297761e1d96f69
seelesi09 <seelesi09@gmail.com>
2026-07-20 19:44:30 +0700

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -30,7 +30,7 @@ const Login = ({ onLoginSuccess, onSwitchToSignup }) => {
     setLoading(true);
 
     try {
-      const response = await axios.post('https://https://theerasstore-production.up.railway.app/api/login', {
+      const response = await axios.post('https://theerasstore-production.up.railway.app/api/login', {
         username: username.trim(),
         password: password.trim()
       });
