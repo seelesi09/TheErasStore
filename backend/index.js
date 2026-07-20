5395a8d9e8b5c64650f8de2dd48ae7f9eeed86df
seelesi09 <seelesi09@gmail.com>
2026-07-20 16:01:30 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -84,10 +84,12 @@ const uploadAudio = multer({
 });
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'ecommerce_mini'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false } // Aiven & banyak cloud MySQL wajib SSL
 });
 
 // Koneksi MySQL
