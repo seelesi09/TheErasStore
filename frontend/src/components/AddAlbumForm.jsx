ec0a527b7300104d1dad0e16be3601e9cabe8a6f
seelesi09 <seelesi09@gmail.com>
2026-07-20 18:50:58 +0700

    Update AddAlbumForm.jsx

--- frontend/src/components/AddAlbumForm.jsx
+++ frontend/src/components/AddAlbumForm.jsx
@@ -53,7 +53,7 @@ export default function AddAlbumForm({ onSuccess }) {
     formData.append("audio", audioFile);
 
     try {
-      const res = await fetch('http://localhost:5000/api/albums', {
+      const res = await fetch('theerasstore-production.up.railway.app/api/albums', {
         method: "POST",
         body: formData,
       });
